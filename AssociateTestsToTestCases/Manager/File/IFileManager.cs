9486f7ef9bca3ad6a2bc0e0415f5e3aae0f63c75
Kate Reeher <kreeher@korterra.com>
2026-01-16 15:42:20 -0600

    Fix IFileManager interface to make allowDuplicates optional
    Make allowDuplicates parameter optional with default false to maintain backward compatibility with existing tests

--- AssociateTestsToTestCases/Manager/File/IFileManager.cs
+++ AssociateTestsToTestCases/Manager/File/IFileManager.cs
@@ -6,7 +6,7 @@ namespace AssociateTestsToTestCases.Manager.File
     public interface IFileManager
     {
         bool TestMethodAssembliesContainNoTestMethods(string[] testAssemblyPaths);
-        TestMethod[] GetTestMethods(string[] testAssemblyPaths, bool allowDuplicates, bool expandParameterizedTests = false);
+        TestMethod[] GetTestMethods(string[] testAssemblyPaths, bool allowDuplicates = false, bool expandParameterizedTests = false);
         string[] GetTestAssemblyPaths(string directory, string[] minimatchPatterns);
     }
 }
