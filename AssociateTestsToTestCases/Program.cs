3022e474992d519596da221590148a17dc92d6dc
Kate Reeher <kreeher@korterra.com>
2026-01-19 09:04:52 -0600

    forgot the import

--- AssociateTestsToTestCases/Program.cs
+++ AssociateTestsToTestCases/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 using System.Diagnostics;
+using System.Reflection;
 using AssociateTestsToTestCases.Message;
 using AssociateTestsToTestCases.Parsing;
 using Microsoft.TeamFoundation.Core.WebApi;
